3e8a600d2a30f3cee444f2e06127f39720e28d5a
GanievaSabina202 <61120111+GanievaSabina202@users.noreply.github.com>
2022-04-24 18:18:17 +0400

    Our Team change card fixed

--- features/our-team/OurTeam.js
+++ features/our-team/OurTeam.js
@@ -59,9 +59,7 @@ export default function OurTeam() {
                         </div>
                     </div>
                 </div>
-
             </div>
-            
         </>
     )
 }
--- share/components/Header/Header.js
+++ share/components/Header/Header.js
@@ -9,20 +9,22 @@ export default function Header() {
         <>
             <Headers>
                 <Logo>
-                    <Img
-                        src='https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=700&quality=85&auto=format&fit=max&s=fb1dca6cdd4589cd9ef2fc941935de71' />
-                    <DensityMediumIcons
-                        onClick={() => {
-                            setNavbar(!Navbar)
-                        }}
-                    />
+                    <Hreff href="/">
+                        <Img
+                            src='https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=700&quality=85&auto=format&fit=max&s=fb1dca6cdd4589cd9ef2fc941935de71' />
+                        <DensityMediumIcons
+                            onClick={() => {
+                                setNavbar(!Navbar)
+                            }}
+                        />
+                    </Hreff>
                 </Logo>
                 <HeaderListWeb>
                     <Hreff href="/">Home</Hreff>
                     <Hreff href="/">Gallery</Hreff>
-                    <Hreff href="/">About Us</Hreff>
-                    <Hreff href="/">Our Team</Hreff>
-                    <Hreff href="/">Contact Us</Hreff>
+                    <Hreff href="about-us">About Us</Hreff>
+                    <Hreff href="our-team">Our Team</Hreff>
+                    <Hreff href="contact-us">Contact Us</Hreff>
                 </HeaderListWeb>
                 {Navbar &&
                     <HeaderList>
