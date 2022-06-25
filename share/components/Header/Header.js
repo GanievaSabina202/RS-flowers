261c09956db5f4237707667fe2ce301a1442ef63
GanievaSabina202 <61120111+GanievaSabina202@users.noreply.github.com>
2022-06-25 22:57:32 +0400

    fixed

--- share/components/Header/Header.js
+++ share/components/Header/Header.js
@@ -1,39 +1,45 @@
-import React, { useState } from 'react';
-import { Headers, Logo, HeaderList, HeaderListWeb, Img, Hreff, DensityMediumIcons } from './Header.styled';
+import React, { useState } from "react";
+import {
+  Headers,
+  Logo,
+  HeaderList,
+  HeaderListWeb,
+  Img,
+  Hreff,
+  DensityMediumIcons,
+} from "./Header.styled";
 
 export default function Header() {
+  const [Navbar, setNavbar] = useState(false);
 
-    const [Navbar, setNavbar] = useState(false)
-
-    return (
-        <>
-            <Headers>
-                <Logo href="/">
-                    <Img
-                        src='/images/logo.png' alt='nsb' />
-                    <DensityMediumIcons
-                        onClick={() => {
-                            setNavbar(!Navbar)
-                        }}
-                    />
-                </Logo>
-                <HeaderListWeb>
-                    <Hreff href="/">Home</Hreff>
-                    <Hreff href="gallery">Gallery</Hreff>
-                    <Hreff href="aboutus">About Us</Hreff>
-                    <Hreff href="our-team">Our Team</Hreff>
-                    <Hreff href="contact-us">Contact Us</Hreff>
-                </HeaderListWeb>
-                {Navbar &&
-                    <HeaderList>
-                        <Hreff href="/">Home</Hreff>
-                        <Hreff href="/">Gallery</Hreff>
-                        <Hreff href="/">About Us</Hreff>
-                        <Hreff href="/">Our Team</Hreff>
-                        <Hreff href="/">Contact Us</Hreff>
-                    </HeaderList>
-                }
-            </Headers>
-        </>
-    )
+  return (
+    <>
+      <Headers>
+        <Logo href="/">
+          <Img src="/images/logo.png" alt="nsb" />
+          <DensityMediumIcons
+            onClick={() => {
+              setNavbar(!Navbar);
+            }}
+          />
+        </Logo>
+        <HeaderListWeb>
+          <Hreff href="/">Home</Hreff>
+          <Hreff href="/gallery">Gallery</Hreff>
+          <Hreff href="/aboutus">About Us</Hreff>
+          <Hreff href="/our-team">Our Team</Hreff>
+          <Hreff href="/contact-us">Contact Us</Hreff>
+        </HeaderListWeb>
+        {Navbar && (
+          <HeaderList>
+            <Hreff href="/">Home</Hreff>
+            <Hreff href="/">Gallery</Hreff>
+            <Hreff href="/">About Us</Hreff>
+            <Hreff href="/">Our Team</Hreff>
+            <Hreff href="/">Contact Us</Hreff>
+          </HeaderList>
+        )}
+      </Headers>
+    </>
+  );
 }
